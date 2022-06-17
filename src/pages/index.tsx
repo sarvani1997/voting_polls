1a7d5e3ba6952d79a85af2509dfc32579ede2656
Sarvani Sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-06-18 01:32:28 +0530

    reformat backend code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,10 @@ import styles from "../styles/Home.module.css";
 import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
-export default function Home(props: any) {
-  const { data, isLoading } = trpc.useQuery(["getAllQuestions"]);
+export default function Home() {
+  const { data, isLoading } = trpc.useQuery(["questions.get-all"]);
 
   if (isLoading || !data) return <div>Loading ....</div>;
 
   return <div>{data[0].question}</div>;
 }
-
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestions.findMany();
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
-};
